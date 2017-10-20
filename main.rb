15ee657467971cb4c87c609a244d319a23497c20
Masato Doi <bridgehead33@gmail.com>
2017-10-20 01:19:42 +0000

    count star from api

--- main.rb
+++ main.rb
@@ -9,12 +9,12 @@ get "/" do
 
   erb :index
 end
-
-uri = URI.parse('http://s.hatena.com/entry.json?https://anond.hatelabo.jp/20171014230038')
+url = "https://anond.hatelabo.jp/20171019123120"
+uri = URI.parse("http://s.hatena.com/entry.json?uri=#{URI.escape(url)}")
 json = Net::HTTP.get(uri)
-result = JSON.parse(json)
-
-puts result
+nomal_stars = JSON.parse(json)["entries"][0]["stars"]
+colored_stars = JSON.parse(json)["entries"][0]["colored_stars"]
+puts nomal_stars
 p "end"
 
 
